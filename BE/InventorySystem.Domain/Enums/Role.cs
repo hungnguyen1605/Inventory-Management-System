7d28c34937c2d00ae003ff988adc6db9c4c8e929
hungnguyen <nguyenvanhung15052003@gmail.com>
2026-03-26 20:53:10 +0700

    add library and add Auth entity

--- BE/InventorySystem.Domain/Enums/Role.cs
+++ BE/InventorySystem.Domain/Enums/Role.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InventorySystem.Application
+namespace InventorySystem.Domain.Enums
 {
-    public class Class1
+    public enum Role
     {
+        Admin,
+        Staff
     }
 }
